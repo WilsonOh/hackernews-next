b199b7b1c0b00891f97ac7e08986dbd4c289e3ae
Wilson Oh <e0773510@u.nus.edu>
2023-10-30 14:26:23 +0800

    chore: add simple logging for backend api calls

--- src/lib/hackernews/hackernews.service.ts
+++ src/lib/hackernews/hackernews.service.ts
@@ -3,24 +3,32 @@ import { Category, apiBaseUrl } from "@/utils/constants";
 import { logger } from "@/utils/logger";
 
 export async function getItem(id: number): Promise<Item | null> {
+  logger.info(`Retrieving item ${id}`);
   const data = await fetch(`${apiBaseUrl}/item/${id}.json`).then((res) =>
     res.ok ? res.json() : null
   );
   if (!data) {
+    logger.error(`Failed to retrieve item ${id}`);
     return null;
   }
   const parseResult = ItemSchema.safeParse(data);
   if (parseResult.success) {
+    logger.info(`Successfully retrieved item ${id}`);
+    logger.debug({ item: parseResult.data });
     return parseResult.data;
   }
-  logger.warn(parseResult.error.formErrors, "Failed to parse item");
+  logger.warn(parseResult.error.formErrors, `Failed to parse item ${id}`);
   return null;
 }
 
 export async function getStories(category: Category): Promise<number[]> {
-  return await fetch(`${apiBaseUrl}/${category}stories.json`).then((res) =>
-    res.json()
+  logger.info(`Retrieving ${category} stories`);
+  const stories = await fetch(`${apiBaseUrl}/${category}stories.json`).then(
+    (res) => res.json()
   );
+  logger.info(`Successfully retrieved ${category} stories`);
+  logger.debug({ stories });
+  return stories;
 }
 
 export async function getMaxItemId(): Promise<number> {
