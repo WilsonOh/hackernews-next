ba020487cd5cc41cd4b68746332919195a1b138b
Wilson Oh <oh.wilson123@gmail.com>
2023-11-12 18:33:06 +0800

    styles: make desktop sidenav width fixed

--- src/components/SideNav/DesktopView.tsx
+++ src/components/SideNav/DesktopView.tsx
@@ -2,7 +2,7 @@ import NavItemList from "./_sub/NavItemList";
 
 export function DesktopSideNav() {
   return (
-    <div className="hidden lg:block border-r border-gray-200 p-2 space-y-2 min-w-[12rem] grow max-w-xs">
+    <div className="hidden lg:block border-r border-gray-200 p-2 space-y-2 min-w-[20rem]">
       <NavItemList />
     </div>
   );
