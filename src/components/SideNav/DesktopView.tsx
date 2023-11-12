8686e6a431696a1ef18da7859ce4b268ed3d10de
Wilson Oh <e0773510@u.nus.edu>
2023-11-12 16:46:09 +0800

    styles(DesktopSideNav): set min width

--- src/components/SideNav/DesktopView.tsx
+++ src/components/SideNav/DesktopView.tsx
@@ -2,7 +2,7 @@ import NavItemList from "./_sub/NavItemList";
 
 export function DesktopSideNav() {
   return (
-    <div className="hidden lg:block border-r border-gray-200 p-2 space-y-2 basis-[20rem]">
+    <div className="hidden lg:block border-r border-gray-200 p-2 space-y-2 min-w-[16rem]">
       <NavItemList />
     </div>
   );
